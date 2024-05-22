605dbcf5343b0efb213d8778cd334be6cc398ece
Elliot <ellthrowaway2@icloud.com>
2024-05-22 17:35:27 -0400

    scoring logic

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
 let playerScore = 0
 let computerScore = 0
+const playerScoreSpanElement = document.getElementById('player-score')
+const computerScoreSpanElement = document.getElementById('computer-score')
+const roundResultsMsg = document.getElementById('results-msg')
 
 const getRandomComputerResult = () => {
     const options = ['Rock', 'Paper', 'Scissors']
     const i = Math.floor(Math.random() * options.length)
     return options[i]
-}    
+}
 
 const hasPlayerWonTheRound = (player, computer) => {
-    if (
-        (player === 'Rock' && computer === 'Scissors') ||
+    (player === 'Rock' && computer === 'Scissors') ||
         (player === 'Scissors' && computer === 'Paper') ||
         (player === 'Paper' && computer === 'Rock')
-    ) {
-        // playerScore++
-        return true
-    } else {
-        // computerScore++
-        return false
-    }
 }
 
 const getRoundResults = (userOption) => {
     const computerResult = getRandomComputerResult()
-    const roundResult = hasPlayerWonTheRound(userOption, computerResult)
-    return console.log(`Has player won? ${roundResult}`)
+    if (hasPlayerWonTheRound(userOption, computerResult)) {
+        playerScore++
+        return `Player wins! ${userOption} beats ${computerResult}`
+    } else if (userOption === computerResult) {
+        return `It's a tie! Both chose ${userOption}`
+    } else {
+        computerScore++
+        return `Computer wins! ${computerResult} beats ${userOption}`
+    }
 }
 
-console.log(getRoundResults("Rock"));
-console.log("Player Score: ", playerScore, "Computer Score: ", computerScore);
+const showResults = (userOption) => {
+    roundResultsMsg.innerText = getRoundResults(userOption)
+    playerScoreSpanElement.innerText = playerScore
+    computerScoreSpanElement.innerText = computerScore
+}
+
+showResults('Rock')
