f0eaf7d83e93bcdfc719ff53a2015bbe423fb66d
Elliot <ellthrowaway2@icloud.com>
2024-05-22 16:43:45 -0400

    fixed random computer result error

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const getRandomComputerResult = () => {
     const options = ["Rock", "Paper", "Scissors"]
-    let i = Math.floor(Math.random())
+    let i = Math.floor(Math.random() * 2)
     return options[i]
 }
 
 console.log(getRandomComputerResult())
+
+const hasPlayerWonTheRound = (player, computer) => {
+
+}
+
+// console.log(hasPlayerWonTheRound("Rock", "Scissors")); 
+// console.log(hasPlayerWonTheRound("Scissors", "Rock")); 
